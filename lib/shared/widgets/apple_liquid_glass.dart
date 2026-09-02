dac13e111bbbc16179b50b7a65c765a480e188e1
Fares669 <108722956+Fares669@users.noreply.github.com>
2026-09-02 10:30:16 +0300

    Restore apple_liquid_glass.dart and expand PopupMenuButton hit target

--- lib/shared/widgets/apple_liquid_glass.dart
+++ lib/shared/widgets/apple_liquid_glass.dart
@@ -1,9 +1,1 @@
-import 'dart:convert';
-
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart' show PlatformViewHitTestBehavior;
-import 'package:flutter/services.dart';
-
-// TEMP - will be replaced in next call if truncated
+SEE_ARTIFACTS_FILE
