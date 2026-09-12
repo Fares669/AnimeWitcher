230ed1ed7df96a5b03d875b48ad0ab80e4b915ef
Fares669 <108722956+Fares669@users.noreply.github.com>
2026-09-12 22:09:23 +0300

    fix: retain Anime4K Metal FFI symbols

--- scripts/anime4k_metal_capi_install.rb
+++ scripts/anime4k_metal_capi_install.rb
@@ -16,3 +16,33 @@ def install_anime4k_metal_capi(plugin_root:, native_dir:, platform:)
   FileUtils.mkdir_p(destination_dir)
   FileUtils.cp(source, File.join(destination_dir, 'Anime4KMetalCAPI.swift'))
 end
+
+
+ANIME4K_METAL_CAPI_SYMBOLS = %w[
+  animewitcher_anime4k_metal_configure
+  animewitcher_anime4k_metal_status
+  animewitcher_anime4k_metal_telemetry
+  animewitcher_anime4k_metal_set_bypass
+  animewitcher_anime4k_metal_disable
+].freeze
+
+def retain_anime4k_metal_capi_symbols(target)
+  return unless target.name == 'media_kit_video'
+
+  retention_flags = ANIME4K_METAL_CAPI_SYMBOLS.map do |symbol|
+    "-Wl,-u,_#{symbol}"
+  end
+  target.build_configurations.each do |configuration|
+    current = configuration.build_settings['OTHER_LDFLAGS']
+    flags = case current
+            when Array
+              current.dup
+            when nil
+              ['$(inherited)']
+            else
+              [current]
+            end
+    retention_flags.each { |flag| flags << flag unless flags.include?(flag) }
+    configuration.build_settings['OTHER_LDFLAGS'] = flags
+  end
+end
--- scripts/test_anime4k_telemetry_install.rb
+++ scripts/test_anime4k_telemetry_install.rb
@@ -3,12 +3,24 @@
 require 'fileutils'
 require 'tmpdir'
 require_relative 'anime4k_media_kit_patch'
+require_relative 'anime4k_metal_capi_install'
 
 Dir.mktmpdir('anime4k-telemetry-install') do |root|
-  plugin_dir = File.join(root, 'plugin')
+  plugin_dir = File.join(root, 'ios', 'Classes', 'plugin')
   native_dir = File.join(root, 'native')
-  FileUtils.mkdir_p(plugin_dir)
+  FileUtils.mkdir_p(File.join(plugin_dir, 'common'))
   FileUtils.mkdir_p(native_dir)
+
+  {
+    'TextureHW.swift' => '// TextureHW',
+    File.join('common', 'ResizableTextureProtocol.swift') => '// protocol',
+    File.join('common', 'SafeResizableTexture.swift') => '// safe',
+    File.join('common', 'VideoOutput.swift') => '// output',
+  }.each do |relative, content|
+    path = File.join(plugin_dir, relative)
+    FileUtils.mkdir_p(File.dirname(path))
+    File.write(path, "#{content}\n")
+  end
 
   %w[
     Anime4KMetalShader.swift
@@ -16,25 +28,42 @@ Dir.mktmpdir('anime4k-telemetry-install') do |root|
     Anime4KMediaKitBridge.swift
     Anime4KMetalTelemetry.swift
     Anime4KMetalFXScaler.swift
+    Anime4KMetalCAPI.swift
   ].each do |name|
     File.write(File.join(native_dir, name), "// #{name}\n")
   end
 
   anime4k_sync_support_files(plugin_dir: plugin_dir, native_dir: native_dir)
-
-  telemetry = File.join(
-    plugin_dir,
-    'anime4k',
-    'Anime4KMetalTelemetry.swift'
+  install_anime4k_metal_capi(
+    plugin_root: root,
+    native_dir: native_dir,
+    platform: :ios
   )
-  raise 'Anime4K Metal telemetry support was not installed into media_kit' unless File.file?(telemetry)
 
-  metalfx = File.join(
-    plugin_dir,
-    'anime4k',
-    'Anime4KMetalFXScaler.swift'
-  )
-  raise 'Anime4K MetalFX support was not installed into media_kit' unless File.file?(metalfx)
+  %w[Anime4KMetalTelemetry.swift Anime4KMetalFXScaler.swift Anime4KMetalCAPI.swift].each do |name|
+    installed = File.join(plugin_dir, 'anime4k', name)
+    raise "#{name} was not installed into media_kit" unless File.file?(installed)
+  end
 end
+
+FakeConfiguration = Struct.new(:build_settings)
+FakeTarget = Struct.new(:name, :build_configurations)
+configs = [
+  FakeConfiguration.new({ 'OTHER_LDFLAGS' => '$(inherited)' }),
+  FakeConfiguration.new({ 'OTHER_LDFLAGS' => ['$(inherited)', '-framework', 'Mpv'] }),
+]
+target = FakeTarget.new('media_kit_video', configs)
+2.times { retain_anime4k_metal_capi_symbols(target) }
+ANIME4K_METAL_CAPI_SYMBOLS.each do |symbol|
+  flag = "-Wl,-u,_#{symbol}"
+  configs.each do |configuration|
+    flags = Array(configuration.build_settings['OTHER_LDFLAGS'])
+    raise "missing C API linker retention flag: #{flag}" unless flags.count(flag) == 1
+  end
+end
+
+untouched = FakeConfiguration.new({ 'OTHER_LDFLAGS' => '$(inherited)' })
+retain_anime4k_metal_capi_symbols(FakeTarget.new('other_plugin', [untouched]))
+raise 'retention flags leaked to another pod target' unless untouched.build_settings['OTHER_LDFLAGS'] == '$(inherited)'
 
 puts 'Anime4KMetalTelemetryInstallTests: PASS'
