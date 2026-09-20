2c007022eb4c0bf00236bd9b761ad992e4e51379
Fares669 <108722956+Fares669@users.noreply.github.com>
2026-09-20 11:41:50 +0300

    feat(manga): add reader page image

---
+++ lib/features/manga/reader/widgets/manga_page_image.dart
@@ -0,0 +1,44 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/domain/entity/manga.dart';
+import '../../../../shared/widgets/loading_indicator.dart';
+
+typedef MangaPageBuilder = Widget Function(
+  BuildContext context,
+  MangaPage page,
+);
+
+class MangaPageImage extends StatelessWidget {
+  const MangaPageImage({
+    super.key,
+    required this.page,
+    this.fit = BoxFit.fitWidth,
+    this.expand = false,
+  });
+
+  final MangaPage page;
+  final BoxFit fit;
+  final bool expand;
+
+  @override
+  Widget build(BuildContext context) {
+    final image = CachedNetworkImage(
+      imageUrl: page.imageUrl,
+      httpHeaders: page.headers,
+      width: double.infinity,
+      height: expand ? double.infinity : null,
+      fit: fit,
+      placeholder: (_, __) => SizedBox(
+        height: expand ? null : 360,
+        child: const Center(child: AppLoadingIndicator()),
+      ),
+      errorWidget: (_, __, ___) => SizedBox(
+        height: expand ? null : 280,
+        child: const Center(child: Icon(Icons.broken_image_outlined, size: 42)),
+      ),
+    );
+
+    return expand ? SizedBox.expand(child: image) : image;
+  }
+}
