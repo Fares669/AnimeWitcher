1c1423b97d5c3c334ab72393a101bd1f6b433bcb
Fares669 <108722956+Fares669@users.noreply.github.com>
2026-09-21 09:54:17 +0300

    fix(manga): import localized title-copy text helper

--- lib/features/manga/presentation/manga_details_screen.dart
+++ lib/features/manga/presentation/manga_details_screen.dart
@@ -13,6 +13,7 @@ import '../../../core/router/app_router.dart';
 import '../../../core/services/notification_service.dart';
 import '../../../core/storage/library_category.dart';
 import '../../../core/utils/image_fallbacks.dart';
+import '../../../core/utils/localized_text.dart';
 import '../../../core/utils/window_controls_inset.dart';
 import '../../../l10n/generated/app_localizations.dart';
 import '../../../shared/widgets/apple_liquid_glass.dart';
